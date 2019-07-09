eb318bfd0bf3a209adaed78582f7af4646af1557
trungdoan1707 <duytrung177@gmail.com>
2019-07-09 09:48:39 +0700

    finish

--- src/main/java/com/codegym/controller/StudentController.java
+++ src/main/java/com/codegym/controller/StudentController.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class StudentController {
     private StudentService studentService = new StudentServiceImpl();
 
-
+    //Phương thức index() hiển thị trang index với danh sách cho sẵn
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(){
         List<Student> students = studentService.findAll();
@@ -26,13 +26,14 @@ public class StudentController {
         return modelAndView;
     }
 
-    //in requestMapping method.get is default
+    //Trong requestMapping method.get là mặc định
     @RequestMapping(value = "/Student/create")
     public ModelAndView create() {
         ModelAndView modelAndView = new ModelAndView("create","student",new Student());
         return modelAndView;
     }
 
+    //Xử lý request method post từ Student/create
     @RequestMapping(value="/Student/create",method = RequestMethod.POST)
     public String create(@ModelAttribute("student") Student student, RedirectAttributes redirect){
         int id = (int) (Math.random() * 10000);
@@ -42,12 +43,14 @@ public class StudentController {
         return "redirect:/";
     }
 
+    //hiển thị trang edit
     @RequestMapping(value = "/Student/edit/{id}")
-    public ModelAndView edit(@PathVariable int id, Model model) {
+    public ModelAndView edit(@PathVariable int id) {
         ModelAndView modelAndView = new ModelAndView("edit", "student",studentService.findById(id) );
         return modelAndView;
     }
 
+    //update thông tin student 
     @RequestMapping(value="/Student/edit/{id}",method = RequestMethod.POST)
     public String edit(@ModelAttribute("student") Student student,RedirectAttributes redirect){
         studentService.update(student.getId(), student);
@@ -55,12 +58,14 @@ public class StudentController {
         return "redirect:/";
     }
 
+    //Hiện thị trang delete
     @RequestMapping("/Student/delete/{id}")
     public ModelAndView delete(@PathVariable int id, Model model) {
         ModelAndView modelAndView = new ModelAndView("delete","student",studentService.findById(id));
         return modelAndView;
     }
 
+    //delete student
     @RequestMapping(value = "/Student/delete/{id}", method = RequestMethod.POST)
     public String delete(Student student, RedirectAttributes redirect) {
         studentService.remove(student.getId());
